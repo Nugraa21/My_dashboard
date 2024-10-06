27e8e0abd3810f0d791e829a6e497f886ebe4d31
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2024-10-06 14:00:22 +0700

    loading img

--- loading/loading.js
+++ loading/loading.js
@@ -1,4 +1,13 @@
-setTimeout(function() {
-    document.getElementById('loader-container').style.display = 'none';
-    // document.getElementById('content').style.display = 'block';
-}, 1000);
+    // Tunggu sampai gambar dimuat
+    const imgCoverLogo = document.querySelector('.img-cover-logo');
+    imgCoverLogo.onload = function() {
+        setTimeout(function() {
+            document.getElementById('loader-container').style.display = 'none';
+            // document.getElementById('content').style.display = 'block';
+        }, 1000);
+    };
+
+    // Jika gambar sudah dalam cache, panggil fungsi langsung
+    if (imgCoverLogo.complete) {
+        imgCoverLogo.onload();
+    }
