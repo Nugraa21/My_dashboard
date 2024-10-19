f6b9aef0ddf7ce3ebbde63053c2cf74afae6d558
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2024-10-19 12:32:25 +0700

    -

--- loading/loading.js
+++ loading/loading.js
@@ -1,13 +1,20 @@
-    // Tunggu sampai gambar dimuat
-    const imgCoverLogo = document.querySelector('.img-cover-logo');
-    imgCoverLogo.onload = function() {
-        setTimeout(function() {
-            document.getElementById('loader-container').style.display = 'none';
-            // document.getElementById('content').style.display = 'block';
-        }, 1000);
-    };
+// Tunggu sampai gambar dimuat
+const imgCoverLogo = document.querySelector('.img-cover-logo');
 
-    // Jika gambar sudah dalam cache, panggil fungsi langsung
-    if (imgCoverLogo.complete) {
-        imgCoverLogo.onload();
-    }
+imgCoverLogo.onload = function() {
+    setTimeout(function() {
+        const loaderContainer = document.getElementById('loader-container');
+        loaderContainer.classList.add('hide'); // Add the hide class for transition
+
+        // Jika loader container masih ada di DOM, sembunyikan setelah transisi
+        loaderContainer.addEventListener('transitionend', function() {
+            loaderContainer.style.display = 'none'; // Hide the container after transition
+            document.getElementById('content').style.display = 'block'; // Show the main content
+        });
+    }, 1000); // Adjust this time as needed
+};
+
+// Jika gambar sudah dalam cache, panggil fungsi langsung
+if (imgCoverLogo.complete) {
+    imgCoverLogo.onload();
+}
