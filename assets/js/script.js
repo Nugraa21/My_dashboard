b66f4af15a271ea11d4416e436affa3ab8a7ceab
Nugra21 <150422561+Nugraa21@users.noreply.github.com>
2024-10-19 16:00:00 +0700

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -189,3 +189,29 @@ toggleButton.addEventListener('click', function() {
         // toggleButton.textContent = "Hide All"; // Ganti teks tombol
     }
 });
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+    elemToggleFunc(skillsBox);
+
+  });
+}
